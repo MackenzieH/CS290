985b2a9deca7d8049b10e53f1e0a2660600893bf
MackenzieH <MackenzieHolznecht@gmail.com>
2016-03-01 09:58:46 -0500

    Update

---
+++ Week-5/script.js
@@ -0,0 +1,130 @@
+/*create the table*/
+var newTable = document.createElement("table");
+//create rows
+for (var i = 0; i < 4; ++i){
+	var tr = document.createElement("tr");
+	//create data
+	for (var j = 1; j < 5; ++j){
+		//create headers
+		if(i == 0){
+			var th = document.createElement("th");
+			th.textContent = "Header " + j ;
+			styleTable(th);
+			tr.appendChild(th);
+		}
+		//create data cells 
+		else {
+			var td = document.createElement("td");
+			td.textContent= i + "," + j;
+			styleTable(td);
+			tr.appendChild(td);	
+		}
+	}
+	//append rows
+	newTable.appendChild(tr);
+}
+newTable.id = "userTable";
+styleTable(newTable);
+document.body.appendChild(newTable);
+
+function styleTable(el){
+	el.style.border = "1px solid black";
+	el.style.textAlign = "center";
+	el.style.marginLeft = "10px";
+}
+
+/*create 4 buttons*/
+var directionButtons = document.createElement("div");
+for(var i = 0; i < 4; ++i){
+	var newButton = document.createElement("button");
+	styleButton(newButton);
+	switch(i){
+		case 0: newButton.textContent = "Up";
+				newButton.id = "upButton";
+				directionButtons.appendChild(newButton);
+				break;
+		case 1: newButton.textContent = "Down";
+				newButton.id = "downButton";
+				directionButtons.appendChild(newButton);
+				break;
+		case 2: newButton.textContent = "Left";
+				newButton.id = "leftButton";
+				directionButtons.appendChild(newButton);
+				break;
+		case 3: newButton.textContent = "Right";
+				newButton.id = "rightButton";
+				directionButtons.appendChild(newButton);
+				break;
+	}	
+}
+document.body.appendChild(directionButtons);
+
+/*create Mark Cell Button*/
+var newButton = document.createElement("button");
+newButton.textContent = "Mark Cell";
+newButton.id = "markButton";
+styleButton(newButton);
+document.body.appendChild(newButton);
+
+//create cell "array"
+var cells = document.getElementById("userTable").getElementsByTagName("td");
+ 
+function styleButton(nb){
+	nb.style.border = "1px solid black";
+	nb.style.marginLeft = "10px";
+	nb.style.marginTop = "20px";
+	nb.style.backgroundColor = "red";
+	nb.style.color = "white";
+}
+
+/*Button Click Functions*/
+//set current
+var current = 0;
+//set upper left cell as current on load
+document.addEventListener("DOMContentLoaded", selectCell);
+
+document.getElementById("upButton").addEventListener("click", function selectUp(){
+	if(current > 3){
+		deselectCell();
+		current = current - 4;
+		selectCell(); 
+	} //else {console.log("Too Far");}
+});
+
+document.getElementById("downButton").addEventListener("click", function selectDown(){
+	if (current < 8){
+		deselectCell();
+		current = current + 4;
+		selectCell(); 
+	} //else {console.log("Too Far");}
+});
+
+document.getElementById("leftButton").addEventListener("click", function selectLeft(){
+	if((current%4 != 0)){
+		deselectCell();
+		current = current - 1;
+		selectCell();
+	} //else {console.log("Too Far");} 
+});
+
+document.getElementById("rightButton").addEventListener("click", function selectRight(){
+	if((current+1)%4 != 0){ 
+		deselectCell();
+		current = current + 1;
+		selectCell();				
+	} //else {console.log("Too Far");}
+});
+
+document.getElementById("markButton").addEventListener("click", function markCell(){
+	cells[current].style.backgroundColor = "yellow"; 
+});
+
+/*Helper Functions*/
+function selectCell(){
+	cells[current].style.outlineStyle = "solid";
+	cells[current].style.outlineWidth = "4px";
+}
+
+function deselectCell(){
+	cells[current].style.outlineStyle = "none";
+}
